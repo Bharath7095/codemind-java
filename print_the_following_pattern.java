b1e63956025483d6406b47c10179bf297bba6b78
Bharath7095 <96734418+Bharath7095@users.noreply.github.com>
2022-09-24 15:11:55 +0530

    import java.util.*; public class file {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n,k;         n=sc.nextInt();         int i,j;         k=n;         for(i=1;i<=n;i++)         {             for(j=1;j<=n;j++)             {                 if(i==j)                 {                     System.out.print("x"+"");                     if(i==k)                     {                         k--;                     }                 }                 else if(k==j)                 {                     System.out.print("x"+"");                     k--;                 }                 else                 {                     System.out.print("0"+"");                 }             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -4,20 +4,30 @@ public class file
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int n;
+        int n,k;
         n=sc.nextInt();
         int i,j;
+        k=n;
         for(i=1;i<=n;i++)
         {
             for(j=1;j<=n;j++)
             {
                 if(i==j)
                 {
-                    System.out.print("0"+"");
+                    System.out.print("x"+"");
+                    if(i==k)
+                    {
+                        k--;
+                    }
+                }
+                else if(k==j)
+                {
+                    System.out.print("x"+"");
+                    k--;
                 }
                 else
                 {
-                    System.out.print("x"+"");
+                    System.out.print("0"+"");
                 }
             }
             System.out.println();
