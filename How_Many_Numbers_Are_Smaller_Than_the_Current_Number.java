0005ca56bfb40b900e607ae004dc3c7761721daf
Bharath7095 <96734418+Bharath7095@users.noreply.github.com>
2022-09-29 13:27:36 +0530

    import java.util.*; public class file {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n,i;         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             int c=0;             for(int j=0;j<n;j++)             {                 if(arr[j]<arr[i])                 {                     c++;                 }             }             System.out.print(c+" ");         }     } }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class file
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i;
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            int c=0;
+            for(int j=0;j<n;j++)
+            {
+                if(arr[j]<arr[i])
+                {
+                    c++;
+                }
+            }
+            System.out.print(c+" ");
+        }
+    }
+}
