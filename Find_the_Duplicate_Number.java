633adf11e5a96bb73bd19947333cd6ddc3383a97
Bharath7095 <96734418+Bharath7095@users.noreply.github.com>
2022-10-15 20:52:20 +0530

    import java.util.*; public class file {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n,i,j;         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             int c=0;             for(j=0;j<n;j++)             {                 if(arr[i]==arr[j])                 {                     c+=1;                 }             }             if(c==2)             {                 System.out.println(arr[i]);                 break;             }         }     } }

---
+++ Find_the_Duplicate_Number.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class file
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            int c=0;
+            for(j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    c+=1;
+                }
+            }
+            if(c==2)
+            {
+                System.out.println(arr[i]);
+                break;
+            }
+        }
+    }
+}
