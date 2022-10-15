c11bb67d67bccf55ecaf40ecb402ae84275a75f8
Bharath7095 <96734418+Bharath7095@users.noreply.github.com>
2022-10-15 12:04:01 +0530

    import java.util.*; public class file {     public static boolean prime(int n)     {         if(n<=1)         {             return false;         }         for(int i=2;i<=Math.sqrt(n);i++)         {             if(n%i==0)             {                 return false;             }         }         return true;     }     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a,b,c=0;         a=sc.nextInt();         b=sc.nextInt();         for(int i=a;i<=b;i++)         {             if(prime(i))             {                 c++;             }         }         System.out.println(c);     } }

---
+++ Primes_in_range.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class file
+{
+    public static boolean prime(int n)
+    {
+        if(n<=1)
+        {
+            return false;
+        }
+        for(int i=2;i<=Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c=0;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(int i=a;i<=b;i++)
+        {
+            if(prime(i))
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
