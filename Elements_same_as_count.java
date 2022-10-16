3f9226de938754317ff63e0d927371bbf88484b7
Bharath7095 <96734418+Bharath7095@users.noreply.github.com>
2022-10-16 11:38:17 +0530

    import java.util.*; public class file{     public static void main(String[] args){         int n,i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int count=0;         for(i=0;i<n;i++)         {             int c=0;             for(int j=0;j<n;j++)             {                 if(arr[i]==arr[j] && i!=j)                 {                     c++;                     arr[j]=0;                 }             }             if(c==arr[i]-1)             {                 System.out.print(arr[i]+" ");                 count++;             }         }         if(count==0)         {             System.out.print("-1");         }     } }

---
+++ Elements_same_as_count.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class file{
+    public static void main(String[] args){
+        int n,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        for(i=0;i<n;i++)
+        {
+            int c=0;
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j] && i!=j)
+                {
+                    c++;
+                    arr[j]=0;
+                }
+            }
+            if(c==arr[i]-1)
+            {
+                System.out.print(arr[i]+" ");
+                count++;
+            }
+        }
+        if(count==0)
+        {
+            System.out.print("-1");
+        }
+    }
+}
